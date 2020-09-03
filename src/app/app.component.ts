de08620fddc84288d2c207f7dc3eaca709e3d611
Owais Ahmed Khan <owais1566@gmail.com>
2020-09-03 15:12:27 +0000

    todo app on angular 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'my-todo-list';
-}
+import { Component } from '@angular/core'; 
+  
+@Component({ 
+    selector: 'app-root', 
+    templateUrl: './app.component.html', 
+    styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent { 
+    title = 'my-todo-list';
+    /* An empty array that is responsible 
+       to add a division */
+    public items = []; 
+  
+    /* A two-way binding performed which 
+       pushes text on division */
+    public newTask; 
+  
+  
+    /* When input is empty, it will 
+       not create a new division */
+    public addToList() { 
+        if (this.newTask == '') { 
+        } 
+        else { 
+            this.items.push(this.newTask); 
+            this.newTask = ''; 
+        } 
+    } 
+  
+    /* This function takes to input the 
+       task, that has to be deleted*/
+    public deleteTask(index) { 
+        this.items.splice(index, 1); 
+    } 
+} 
